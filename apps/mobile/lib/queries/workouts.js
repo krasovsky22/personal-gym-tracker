29815ff3e8fa9a7698965fdf9a787ee0317a2ec4
Vlad Krasovski <krasovsky@kentike.com>
2023-12-31 18:58:29 +0800

    123

--- apps/mobile/lib/queries/workouts.js
+++ apps/mobile/lib/queries/workouts.js
@@ -3,14 +3,6 @@ import { supabase } from '@lib/supabase';
 const WORKOUT_TABLE = 'workout';
 const WORKOUT_EXERCISE_TABLE = 'workout_exercise';
 
-const formdata = {
-  exercises: [
-    { exercise: '662132a4-c9f8-49cc-8717-545e5e3be684', setsCount: 20 },
-    { exercise: 'e727be4d-bdca-4ec8-98b8-9d2cdc4eeffc', setsCount: 3 },
-  ],
-  name: 'Chest',
-};
-
 export async function createWorkout({ name, exercises }) {
   try {
     const workout = {
@@ -39,7 +31,6 @@ export async function createWorkout({ name, exercises }) {
       success: true,
       data: { workout: { ...insertedWorkout, exercises: exercisesSelect } },
     };
-    // return { success: true, data: data[0] };
   } catch (error) {
     console.error('Unable to insert exercise', error);
   }
