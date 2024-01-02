8dadde77414e5bb680d4166fdce505eb72624f50
Vlad Krasovski <krasovsky@kentike.com>
2024-01-02 18:08:53 +0800

    added missing migraiton

---
+++ supabase/migrations/20240102100730_remote_schema.sql
@@ -0,0 +1,53 @@
+create table "public"."profiles" (
+    "id" uuid not null,
+    "updated_at" timestamp with time zone,
+    "username" text,
+    "full_name" text,
+    "avatar_url" text,
+    "website" text
+);
+
+
+alter table "public"."profiles" enable row level security;
+
+CREATE UNIQUE INDEX profiles_pkey ON public.profiles USING btree (id);
+
+CREATE UNIQUE INDEX profiles_username_key ON public.profiles USING btree (username);
+
+alter table "public"."profiles" add constraint "profiles_pkey" PRIMARY KEY using index "profiles_pkey";
+
+alter table "public"."profiles" add constraint "profiles_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON DELETE CASCADE not valid;
+
+alter table "public"."profiles" validate constraint "profiles_id_fkey";
+
+alter table "public"."profiles" add constraint "profiles_username_key" UNIQUE using index "profiles_username_key";
+
+alter table "public"."profiles" add constraint "username_length" CHECK ((char_length(username) >= 3)) not valid;
+
+alter table "public"."profiles" validate constraint "username_length";
+
+create policy "Public profiles are viewable by everyone."
+on "public"."profiles"
+as permissive
+for select
+to public
+using (true);
+
+
+create policy "Users can insert their own profile."
+on "public"."profiles"
+as permissive
+for insert
+to public
+with check ((auth.uid() = id));
+
+
+create policy "Users can update own profile."
+on "public"."profiles"
+as permissive
+for update
+to public
+using ((auth.uid() = id));
+
+
+
