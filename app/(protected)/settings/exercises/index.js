706b60d62167cea3840237851ff394efbfe34bdc
Vlad Krasovski <krasovsky@kentike.com>
2024-05-18 19:18:34 +0800

    added search

--- app/(protected)/settings/exercises/index.js
+++ app/(protected)/settings/exercises/index.js
@@ -1,6 +1,7 @@
 import { toJS } from 'mobx';
+import { useState, useMemo } from 'react';
 import { View, StyleSheet, FlatList } from 'react-native';
-import { useTheme, FAB, ListItem, Icon, Button } from '@rneui/themed';
+import { useTheme, FAB, ListItem, Icon, Button, Input } from '@rneui/themed';
 import { observer } from 'mobx-react-lite';
 import { Stack, useRouter } from 'expo-router';
 
@@ -9,64 +10,80 @@ import { AsyncButton } from '@components';
 import useExercisesStore from '@hooks/useExercisesStore';
 
 function ExercisesScreen() {
+  const [filterText, setFilterText] = useState('');
   const router = useRouter();
   const { theme } = useTheme();
   const { exercises, deleteExercise } = useExercisesStore();
+
+  const filteredExercises = useMemo(() => {
+    const exercisesObjects = toJS(exercises);
+
+    if (!filterText.length) {
+      return exercisesObjects;
+    }
+
+    return exercisesObjects.filter((exercise) =>
+      exercise.name.includes(filterText)
+    );
+  }, [filterText, exercises.length]);
 
   return (
     <View style={styles.container}>
       <Stack.Screen options={{ title: 'Exercises Management' }} />
       <View style={styles.list}>
+        <View>
+          <Input
+            placeholder="Search"
+            onChangeText={setFilterText}
+            leftIcon={{ name: 'magnify', type: 'material-community' }}
+          />
+        </View>
         <FlatList
-          data={toJS(exercises)}
+          data={filteredExercises}
           keyExtractor={(a) => a.id}
           renderItem={({ item }) => (
-            <View>
-              <ListItem.Swipeable
-                bottomDivider
-                leftWidth={80}
-                rightWidth={90}
-                minSlideWidth={40}
-                leftContent={(action) => (
-                  <Button
-                    containerStyle={{
-                      flex: 1,
-                      justifyContent: 'center',
-                      backgroundColor: theme.colors.warning,
-                    }}
-                    type="clear"
-                    icon={{
-                      name: 'pencil',
-                      type: 'material-community',
-                    }}
-                    onPress={() =>
-                      router.push(`/settings/exercises/${item.id}`)
-                    }
-                  />
-                )}
-                rightContent={(action) => (
-                  <AsyncButton
-                    containerStyle={{
-                      flex: 1,
-                      justifyContent: 'center',
-                      backgroundColor: theme.colors.error,
-                    }}
-                    type="clear"
-                    icon={{ name: 'delete-outline' }}
-                    onPress={() => {
-                      return deleteExercise(item.id);
-                    }}
-                  />
-                )}
-              >
-                <Icon name="label-important-outline" type="material" />
-                <ListItem.Content>
-                  <ListItem.Title>{item.name}</ListItem.Title>
-                  <ListItem.Subtitle>Placeholder Description</ListItem.Subtitle>
-                </ListItem.Content>
-                <ListItem.Chevron />
-              </ListItem.Swipeable>
-            </View>
+            <ListItem.Swipeable
+              bottomDivider
+              leftWidth={80}
+              rightWidth={90}
+              minSlideWidth={40}
+              leftContent={() => (
+                <Button
+                  containerStyle={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    backgroundColor: theme.colors.warning,
+                  }}
+                  type="clear"
+                  icon={{
+                    name: 'pencil',
+                    type: 'material-community',
+                  }}
+                  onPress={() => router.push(`/settings/exercises/${item.id}`)}
+                />
+              )}
+              rightContent={() => (
+                <AsyncButton
+                  containerStyle={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    backgroundColor: theme.colors.error,
+                  }}
+                  type="clear"
+                  icon={{ name: 'delete-outline' }}
+                  onPress={() => {
+                    return deleteExercise(item.id);
+                  }}
+                />
+              )}
+            >
+              <Icon name="label-important-outline" type="material" />
+              <ListItem.Content>
+                <ListItem.Title>{item.name}</ListItem.Title>
+                <ListItem.Subtitle>Placeholder Description</ListItem.Subtitle>
+              </ListItem.Content>
+              <ListItem.Chevron />
+            </ListItem.Swipeable>
           )}
         />
         <AddNewFab url="/settings/exercises/new" />
@@ -77,6 +94,7 @@ function ExercisesScreen() {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'white',
     display: 'flex',
     flex: 1,
   },
