0ed91d04c41f4fffde6bcd9154721d8029c637d7
Vlad Krasovski <krasovsky@kentike.com>
2024-06-20 14:53:10 +0800

    new design

--- app/(protected)/user-workouts/track-workout/components/user-workout-tracker.tsx
+++ app/(protected)/user-workouts/track-workout/components/user-workout-tracker.tsx
@@ -12,12 +12,23 @@ const UserWorkoutTracker = ({ userWorkout }: UserWorkoutTrackerType) => {
         <FlatList
           data={userWorkout.userWorkoutExercises}
           renderItem={({ item }) => (
-            <View>
-              <Text>{item.exercise?.name}</Text>
+            <View style={styles.userWorkoutContainer}>
+              <View style={styles.userWorkoutContainerTitle}>
+                <Text>{item.exercise?.name}</Text>
+                <Text>Placeholder</Text>
+              </View>
+
+              <View style={styles.userWorkoutSetsHeader}>
+                <Text style={styles.userWorkoutSetHeaderText}>SET</Text>
+                <Text style={styles.userWorkoutSetHeaderText}>PREV</Text>
+                <Text style={styles.userWorkoutSetHeaderText}>REP</Text>
+                <Text style={styles.userWorkoutSetHeaderText}>KG</Text>
+                <Text style={styles.userWorkoutSetHeaderText}></Text>
+              </View>
               <FlatList
                 data={item.userWorkoutExerciseSets}
                 renderItem={({ item: exerciseSet }) => (
-                  <View style={{ flex: 1 }}>
+                  <View style={styles.setRowContainer}>
                     <Text>Weight: </Text>
                     <Text>{exerciseSet.weight}</Text>
                     <Text>Repeats: </Text>
@@ -40,6 +51,34 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   titleContainer: {
+    flex: 1,
+  },
+  userWorkoutContainer: {
+    backgroundColor: 'orange',
+  },
+
+  userWorkoutContainerTitle: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  userWorkoutSetsHeader: {
+    backgroundColor: 'green',
+    flex: 1,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    textAlign: 'center',
+  },
+
+  setRowContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'pink',
+    justifyContent: 'space-evenly',
+  },
+
+  userWorkoutSetHeaderText: {
     flex: 1,
   },
 });
