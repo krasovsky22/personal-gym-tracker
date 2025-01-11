5eea3102c6bf1fa44829f1634a6c86c5945695b6
Vlad Krasovski <krasovsky@kentike.com>
2025-01-11 13:21:12 +0800

    fixed avatar for browser

--- components/UI/avatar.tsx
+++ components/UI/avatar.tsx
@@ -1,5 +1,7 @@
+import { Platform } from 'react-native';
 import { StyleSheet, View } from 'react-native';
 import { SvgUri, UriProps } from 'react-native-svg';
+import { Avatar as RNEAvatar } from 'react-native-elements';
 
 type AvatarProps = UriProps & {
   rounded?: boolean;
@@ -11,12 +13,19 @@ const Avatar = ({
   rounded = false,
   ...props
 }: AvatarProps) => {
+  // react native svg does not work in web
+  const isWeb = Platform.OS === 'web';
+
   return (
     <View style={[rounded ? styles.rounded : {}, { borderRadius: size }]}>
-      <SvgUri
-        {...props}
-        style={[style, { margin: 5, width: size - 5, height: size - 5 }]}
-      />
+      {isWeb ? (
+        <RNEAvatar size={32} rounded source={{ uri: props.uri ?? '' }} />
+      ) : (
+        <SvgUri
+          {...props}
+          style={[style, { margin: 5, width: size - 5, height: size - 5 }]}
+        />
+      )}
     </View>
   );
 };
